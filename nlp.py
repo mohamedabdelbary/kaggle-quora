54de7885171420d6fe27f65fc9394e8e635feac7
Mohamed Abdelbary <mohamed.abdelbary@duedil.com>
2017-03-19 20:05:41 +0000

    basic features from LDA model

--- nlp.py
+++ nlp.py
@@ -28,38 +28,40 @@ def clean_statement(s):
 
 def construct_doc_list(df):
     """
-    Take the question pair df's and return a list of 2 docs per
+    Take the question pairs DF and return a list of 2 docs per
     row with the cleaned up sentence
     """
     for index, row in df.iterrows():
         q1, q2 = row["question1"], row["question2"]
         q1_tokens, q2_tokens = clean_statement(q1), clean_statement(q2)
 
-        q1_doc = [UnicodeDammit(w.lemma_.lower()).markup for w in q1_tokens]
-        q2_doc = [UnicodeDammit(w.lemma_.lower()).markup for w in q2_tokens]
+        q1_doc = [w.lemma_.lower() for w in q1_tokens]
+        q2_doc = [w.lemma_.lower() for w in q2_tokens]
 
         yield q1_doc
         yield q2_doc
 
 
-def train_lda(n_topics, dictionary=None, documents=None, corpus=None):
+def train_lda(n_topics, id2word_dictionary=None, documents=None, corpus=None):
     """
     Training method for LDA. documents is a list of lists of words/tokens
     documents is used to construct a dictionary and a corpus from which the
     topics for LDA are inferred
     """
     # Construct dictionary of words if it's not passed
-    if not dictionary:
-        dictionary = corpora.Dictionary(documents)
+    if not id2word_dictionary:
+        id2word_dictionary = corpora.Dictionary(documents)
+
+    word2idx_dictionary = dict([(w, idx) for (idx, w) in id2word_dictionary.items()])
 
     # Construct corpus for model
     if documents and not corpus:
-        corpus = [dictionary.doc2bow(document) for document in documents]
+        corpus = [id2word_dictionary.doc2bow(document) for document in documents]
 
     # Cluster the documents into topics using LDA. number of topics is given
     # by n_topics
     lda_model = LdaModel(corpus=corpus,
-                         id2word=dictionary,
+                         id2word=id2word_dictionary,
                          num_topics=n_topics,
                          update_every=1,
                          chunksize=10000,
@@ -73,12 +75,29 @@ def train_lda(n_topics, dictionary=None, documents=None, corpus=None):
     topics = [lda_model.show_topic(idx, topn=50000)
               for idx in range(0, n_topics)]
 
-    return lda_model, dictionary, topics
+    return lda_model, id2word_dictionary, word2idx_dictionary, topics
 
 
-def features(row):
+def features(row, lda_model, word2idx_dict, n_lda_topics=10):
     q1, q2 = row["question1"], row["question2"]
     q1_tokens, q2_tokens = clean_statement(q1), clean_statement(q2)
+
+    # LDA related features
+    q1_lda_doc = [w.lemma_.lower() for w in q1_tokens]
+    q2_lda_doc = [w.lemma_.lower() for w in q2_tokens]
+    q1_topic_probs = dict(
+        lda_model.get_document_topics(Counter([word2idx_dict[w] for w in q1_lda_doc]).items())
+    )
+    q2_topic_probs = dict(
+        lda_model.get_document_topics(Counter([word2idx_dict[w] for w in q2_lda_doc]).items())
+    )
+
+    q1_topic_probs = [(t, q1_topic_probs[t]) if t in q1_topic_probs else (t, 0.0) for t in range(n_lda_topics)]
+    q2_topic_probs = [(t, q2_topic_probs[t]) if t in q2_topic_probs else (t, 0.0) for t in range(n_lda_topics)]
+
+    q1_topic_vector = np.array([prob for (topic, prob) in q1_topic_probs])
+    q2_topic_vector = np.array([prob for (topic, prob) in q2_topic_probs])
+    diff_topic_vector = q1_topic_vector - q2_topic_vector
 
     q1_doc = nlp(UnicodeDammit(' '.join([w.lemma_.lower() for w in q1_tokens])).markup) if q1_tokens else None
     q2_doc = nlp(UnicodeDammit(' '.join([w.lemma_.lower() for w in q2_tokens])).markup) if q2_tokens else None
@@ -100,7 +119,12 @@ def features(row):
         dot_product = q1_vector.dot(q2_vector) 
         cosine_sim = cosine_similarity(q1_vector, q2_vector)[0][0]
         euclidean_dist = np.linalg.norm(q1_vector - q2_vector)
+        euclidean_lda_probs_dist = np.linalg.norm(diff_topic_vector)
     else:
-        dot_product = cosine_sim = euclidean_dist = 0.0
+        dot_product = cosine_sim = 0.0
+        euclidean_dist = euclidean_lda_probs_dist = 100.0
 
-    return [token_overlap_ratio, dot_product, cosine_sim, euclidean_dist]
+    feature_list = [token_overlap_ratio, dot_product, cosine_sim, euclidean_dist, euclidean_lda_probs_dist]
+    feature_list.extend(list(diff_topic_vector))
+
+    return feature_list
