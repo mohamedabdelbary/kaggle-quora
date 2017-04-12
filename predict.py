07e3bba55b8a542df35d7c120d7c335b7b5822f0
Mohamed Abdelbary <mohamed.abdelbary@duedil.com>
2017-04-12 11:12:49 +0100

    updated predict script

--- predict.py
+++ predict.py
@@ -1,47 +1,35 @@
-test_path = "/Users/mohamedabdelbary/Documents/kaggle_quora/test.csv"
-models_path = "/Users/mohamedabdelbary/Documents/kaggle_quora/models_v1_with_oversampling.pkl"
-output_path = "/Users/mohamedabdelbary/Documents/kaggle_quora/test_predictions_v1.csv"
-
-
+import sys
 import pickle
 import numpy as np
 import pandas
 from functools import partial
-from nlp import features, construct_doc_list, train_lda
-from model import binary_logloss, RandomForestModel, predict_rf
+from train_config import train_features
+from model import binary_logloss, RandomForestModel, predict_rf, predict_xgboost
 
 
 if __name__ == "__main__":
-    test_df = pandas.read_csv(test_path)
+    test_set_features_path = sys.argv[1]
+    model_path = sys.argv[2]
+    output_path = sys.argv[3]
+    relevant_cols = ["test_id"] + train_features
+    
+    print "Loading test dataset with features!"
+    test_df = pandas.read_csv(test_set_features_path)[relevant_cols]
+    print "<===================================>"
+
     print "Loading models!"
-    with open(models_path, 'rb') as models_file:
-        models = pickle.load(models_file)
-
-    print "Starting feature construction!"
-    feature_method = partial(
-        features,
-        lda_model=models["lda"],
-        word2idx_dict=models["word2idx_dict"],
-        n_lda_topics=len(models["topics"]),
-        word_weights=models["word_weights"])
-
-    # test_df["features"] = test_df.apply(feature_method, axis=1)
-    test_df = feature_method(test_df)
-
-    # No need to keep raw data columns or the LDA model now.
-    # Removing to reduce memory pressure
-    test_df.drop('question1', axis=1, inplace=True)
-    test_df.drop('question2', axis=1, inplace=True)
-    models.pop("lda")
-    models.pop("word2idx_dict")
-    models.pop("topics")
-    models.pop("word_weights")
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
 
     print  "<==================================>"
     print "Starting predictions!"
 
-    predict_method = partial(predict_rf, model=models["rf"])
-    test_df["is_duplicate"] = test_df.apply(predict_method, axis=1)
+    if model['type'] == 'rf':
+        predict_method = partial(predict_rf, model=model["model"], feature_cols=train_features)
+        test_df["is_duplicate"] = test_df.apply(predict_method, axis=1)
+    elif model['type'] == 'xgb':
+        predict_method = partial(predict_xgboost, model=model["model"], feature_cols=train_features)
+        test_df["is_duplicate"] = test_df.apply(predict_method, axis=1)
 
     print "<===================================>"
     print "Finished predictions. Saving output!"
